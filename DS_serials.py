f4a69ae0da9ab77cb9e0510c3b946efb1aaf2e03
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-06-04 13:15:58 +0100

    settings

--- DS_serials.py
+++ DS_serials.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import sys
+import sys, os
 try:
 	import serial
 	from serial.tools.list_ports import comports
@@ -10,23 +10,57 @@ except ImportError:
 
 class Serial:
 	def __init__(self):
-		ports = list()
-		descs = list()
-		for port,desc,hwid in comports():
-			ports.append(port)
-			descs.append(desc)
-		if(len(descs) > 1):
-			print('More than 1 serial port found.')
-			for i in range(0,len(ports)):
-				print('   %s' % descs[i])
-			self.connect(ports[1],9600,8,1,serial.PARITY_NONE,0)
-		else:
-			print('Connecting to only available serial port.')
-			print('   %s' % descs[1])
-	def connect(self,p,bd,bs,p,s,t,parity,rtscts):
+		# IMPORT STUFF
 		try:
-			self.ser = serial.Serial(com,baudrate,bytesize,timeout,parity,rtscts)
+			import serial
+			from serial.tools.list_ports import comports
+		except ImportError:
+			print('\nError: PySerial not found, exiting...')
+			sys.exit(0)
+		# USER SLECTS PORT
+		while(1):
+			try:
+				ports = list()
+				descs = list()
+				for port,desc,hwid in comports():
+					ports.append(port)
+					descs.append(desc)
+				if(len(descs) == 0):										# NO Ports
+					print('\nError: No serial ports found, exiting...')
+					sys.exit(0)
+				elif(len(descs) == 1):										# One Port
+					print('\nConnecting to only available serial port.')
+					print('   %s' % descs[0])
+					chosen = ports[0]
+					break
+				else:														# Many Ports, choose
+					num_ports = len(ports)
+					print('\nMore than 1 serial port found.')
+					for i in range(0,num_ports):
+						print('%s:   %s' % (i,descs[i]))
+					user = ''
+					user = input('NUM:')
+					chosen = int(user)
+					if((chosen < 0) or (chosen >= num_ports)):
+						raise NameError('ChoiceError')
+					else:
+						break
+			except NameError,ValueError:
+				print('Invalid choice')
+		# LOAD SETTINGS FILE
+		try:
+			f = open("DS_configs.dat", 'r')
+			contents = f.read()
+			if((contents.count('\n') > 1) or (contents.count(',') > 6)):
+				print('Settings corrupt')
+				raise NameError('SettingsError')
+		except:									# Config file is missing or corrupt
+			f = open("DS_configs.dat", 'w')
+			f.write('9600,8,None,1,None,0,0')
+		# CONNECT
+		try:
+			self.ser = serial.Serial(port=ports[chosen],baudrate=9600,bytesize=8,parity='N',stopbits=1,timeout=None,xonxoff=0,rtscts=0)
 		except serial.serialutil.SerialException:
 			print('Error: Connecting to serial port, exiting...')
 			sys.exit(0)
-
+		self.ser.write("sup")
--- DebugSerial.py
+++ DebugSerial.py
@@ -8,25 +8,7 @@ from DS_serials import Serial
 
 def main():
 	u = Serial()
-	#g = Graph(100)
-	#t1 = Thread(target=g.update)
-	#t1.start()
-	#for i in range(1,150):
-	#	time.sleep(0.05)
-	#	dummyData = randrange(0,255,1)
-	#	DS_strings.radix(dummyData)
-	#	g.newXY(i,dummyData)
-	#g.kill()
-	#t1.join()
-	#print 'Finished'
 
-
-def connect(com,baudrate):
-	try:
-		return serial.Serial(com,baudrate,bytesize=8,timeout=1,parity=serial.PARITY_NONE,rtscts=0)
-	except serial.serialutil.SerialException:
-		print('Error: Connecting to serial port, exiting...')
-		sys.exit(0)
 
 if __name__ == "__main__":
 	main()
