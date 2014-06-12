deea09fa6db3fe2fe099a3ef9e9af3baf1c0d42b
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-06-12 19:42:13 +0100

    overheads

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
-DebugSerial
-===========
+TODO
+====
 
-A tool for digital electronics development where a debug serial connection is being used to test software. Cross platform written in python for command line. Simple text interface but also crude graphing and radix conversion.
+Lots and lots
--- setup.py
+++ setup.py
@@ -1,43 +1,13 @@
-#!/usr/bin/python
-import sys
+#!/usr/bin/env python
 
-class Test:
-	def __init__(self,testFile):
-		try:
-			test = testFile + ".csv"
-			f = open(test, 'r')
-			self.send = list()
-			self.get = list()
-			print('Loading %s' % test)
-			num = 1
-			for line in f:
-				data = line.split(',')
-				print data
-				if(num == 1):
-					if((len(data) != 3) or (data[0] != "<DS")):
-						print('\nInvalid .csv file: %s' % data)
-						raise NameError
-					data[1] = data[1].strip('Num=')
-					if(data[1] == 'True'):
-						numbers=True
-					elif(data[1] == 'False'):
-						numbers=False
-					else:
-						print('\nInvalid boolean option for Num: %s' % data[1])
-						raise NameError
-					print('Numbers: %s' % data[1])
-				else:
-					if((len(data) != 2)):													# Two entrie per line
-						print('\nInvalid number of entries on line %s: %s' % (num,line))
-						raise NameError
-					self.send.append(data[0])
-					self.get.append(data[1].strip('\n'))
-				num = num + 1
-			f.close()
-			print self.send
-			print self.get
-		except NameError:
-			print('Failed to load test vectors, exiting...')
-			sys.exit(0)
+from distutils.core import setup
 
+setup(name='DebugSerial',
+      version='1.0',
+      description='A serial debug tool in python',
+      author='Ashley J. Robinson',
+      author_email='ajr2g10@ecs.soton.ac.uk',
+      url='www.ajrobinson.org',
+      packages=['serial'],
+     )
 
