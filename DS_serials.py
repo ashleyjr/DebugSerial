ffe74777a18146343a01fd18e3a902857f70f79c
Ashley Robinson <ashley181291@gmail.com>
2014-06-12 14:57:59 +0100

    F1 hack with state machine

--- DS_serials.py
+++ DS_serials.py
@@ -75,8 +75,8 @@ class Serial:
 
 
 		# LOAD BAUD RATE
-		startBAUD = 1234567								# Warning dynamic update to persist
-		BAUD = startBAUD
+		startBAUD = 100								# Warning dynamic update to persist
+		BAUD = startBAUD							# Redundancy required for persistence
 		while(1):
 			print('\nBaud: %s' % BAUD)
 			sys.stdout.write('Ok? [y/n]')
@@ -116,20 +116,116 @@ class Serial:
 
 	def asyncRx(self):
 		while(self.async == 1):
-			sys.stdout.write(self.ser.read(1))
+			sys.stdout.write(self.humanRead(self.ser.read(1)))
 			sys.stdout.flush()
+		print("\n\n")
 
 	def asyncTx(self):
-		while(1):
+		state = 0
+		f1 = [27,79,80]
+		buff3 = [0,0,0]
+		while(self.async == 1):
 			user = self.char()
 			test = ord(user)
+			if(state == 0):
+				buff3[0] = test
+				state = 1
+			elif(state == 1):
+				buff3[1] = test
+				state = 2
+			else:
+				buff3[2] = test
+				state = 0
+			shift = buff3
+			for i in range(3):
+				temp = shift[0]
+				shift[0] = shift[1]
+				shift[1] = shift[2]
+				shift[2] = temp
+				if(shift == f1):
+					self.async = 0
 			if((test >= 0) and (test <= 255)):
 				self.ser.write(user)
-			if(user == 'q'):
-				break
-		self.async = 0
+
+	def asyncMenu():
 
 
+	def humanRead(self,char):
+		data = ord(char)
+		if(data == 0):
+			return "<Null char>"
+		elif(data == 1):
+			return "<datatart of Heading>"
+		elif(data == 2):
+			return "<Start of Text>"
+		elif(data == 3):
+			return "<End of Text>"
+		elif(data == 4):
+			return "<End of Transmission>"
+		elif(data == 5):
+			return "<Enquiry>"
+		elif(data == 6):
+			return "<Acknowledgment>"
+		elif(data == 7):
+			return "<Bell>"
+		elif(data == 8):
+			return "<Back Space>"
+		elif(data == 9):
+			return "<Horizontal Tab>"
+		elif(data == 10):
+			return "<Line Feed>"
+		elif(data == 11):
+			return "<Vertical Tab>"
+		elif(data == 12):
+			return "<Form Feed>"
+		elif(data == 13):
+			return "\n\r"	#"<Carriage Return>"
+		elif(data == 14):
+			return "<Shift Out / X-On>"
+		elif(data == 15):
+			return "<Shift In / X-Off>"
+		elif(data == 16):
+			return "<Data Line Escape>"
+		elif(data == 17):
+			return "<Device Control 1 (oft. XON)>"
+		elif(data == 18):
+			return "<Device Control 2>"
+		elif(data == 19):
+			return "<Device Control 3 (oft. XOFF)>"
+		elif(data == 20):
+			return "<Device Control 4>"
+		elif(data == 21):
+			return "<Negative Acknowledgement>"
+		elif(data == 22):
+			return "<Synchronous Idle>"
+		elif(data == 23):
+			return "<End of Transmit Block>"
+		elif(data == 24):
+			return "<Cancel>"
+		elif(data == 25):
+			return "<End of Medium>"
+		elif(data == 26):
+			return "<Substitute>"
+		elif(data == 27):
+			return "<Escape>"
+		elif(data == 28):
+			return "<File Separator>"
+		elif(data == 29):
+			return "<Group Separator>"
+		elif(data == 30):
+			return "<Record Separator>"
+		elif(data == 31):
+			return "<Unit Separator>"
+		elif(data == 32):
+			return " "#"<Space>"
+		elif(data == 127):
+			return "\b"
+		else:
+			if((data > 255) or (data < 0)):
+				print("Invalid!")
+				sys.exit(0)
+			else:
+				return char
 
 
 
