5b210662f1848c2fdc565fab72f790236417bed1
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-07-17 00:25:54 +0100

    timer driven

---
+++ TimerDriven/DebugSerial.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+print("______     _                 _____           _       _" )
+print("|  _  \   | |               /  ___|         (_)     | |")
+print("| | | |___| |__  _   _  __ _\ `--.  ___ _ __ _  __ _| |")
+print("| | | / _ \ '_ \| | | |/ _` |`--. \/ _ \ '__| |/ _` | |")
+print("| |/ /  __/ |_) | |_| | (_| /\__/ /  __/ |  | | (_| | |")
+print("|___/ \___|_.__/ \__,_|\__, \____/ \___|_|  |_|\__,_|_|")
+print("                        __/ |                          ")
+print("                       |___/                           ")
+
+import sys
+from serials import Serial
+import optparse
+from PyQt4 import QtGui
+from Multi import Multi
+from Menu import Menu
+
+
+
+
+
+if __name__ == "__main__":
+	parser = optparse.OptionParser()
+	parser.add_option('-b', '--baud',
+		dest="baud"
+	)
+	parser.add_option('-c', '--com',
+		dest="com"
+	)
+	options, remainder = parser.parse_args()
+
+	u = Serial()
+	if(options.baud or options.com):
+		print("User options...")
+	if(options.baud):
+		print 'Baud: ', options.baud
+		u.baud(options.baud)
+	if(options.com):
+		print ' Com: ', options.com
+		u.com(options.com)
+	u.connect()
+	print("\nLaunching GUI")
+	app = QtGui.QApplication(sys.argv)
+	root = Multi()
+	menu = Menu(root,u)
+	app.exec_()
+	u.disconnect()
+
---
+++ TimerDriven/Grap.py
@@ -0,0 +1,112 @@
+import sys
+import numpy
+import threading
+from strings import *
+from PyQt4.Qwt5 import *
+from PyQt4 import QtCore, QtGui, uic
+import PyQt4.Qwt5 as Qwt
+
+numPoints=1000
+xs=numpy.arange(numPoints)
+ys=numpy.sin(3.14159*xs*10/numPoints)
+
+
+
+class Grap(QtGui.QMainWindow):
+	def __init__(self, caller, ser, parent=None):
+		super(Grap, self).__init__()
+
+		uic.loadUi('Grap.ui', self)
+
+		self.setWindowTitle('DS: Graph')
+		self.show()
+		self.ser = ser
+
+		self.caller = caller
+
+
+		self.rxPlot=Qwt.QwtPlotCurve()
+		self.rxPlot.attach(self.qwtPlotRx)
+		self.qwtPlotRx
+
+		self.txPlot=Qwt.QwtPlotCurve()
+		self.txPlot.attach(self.qwtPlotTx)
+		self.qwtPlotTx
+
+		self.timer =  QtCore.QTimer()
+		self.timer.start(100.0)
+
+		self.connect(self.timer, QtCore.SIGNAL('timeout()'), self.output)
+
+		self.connect(self.slider, QtCore.SIGNAL('valueChanged(int)'), self.setAxis)
+
+
+
+
+		self.rx_x = []
+		self.rx_y = []
+		self.tx_x = []
+		self.tx_y = []
+
+		self.rx_count = 0
+		self.tx_count = 0
+
+		self.rx_x.append(0)
+		self.rx_y.append(0)
+		self.tx_x.append(0)
+		self.tx_y.append(0)
+
+		self.textEditTx.keyPressEvent = self.tx
+
+
+		self.show()
+
+	def setAxis(self,value):
+		self.qwtPlotRx.setAxisScale(Qwt.QwtPlot.xBottom, 0., value)
+		print value
+
+	def output(self):
+		global ys
+		self.rxPlot.setData(self.rx_x, self.rx_y)
+		self.qwtPlotRx.replot()
+		self.txPlot.setData(self.tx_x, self.tx_y)
+		self.qwtPlotTx.replot()
+		self.rx()
+
+
+	def tx(self, e):
+		key = unicode(e.text())
+		if(len(key) != 0):
+			self.textEditTx.insertPlainText(humanRead(key,text=True))
+			self.textEditTx.ensureCursorVisible()
+			data = ord(key)
+			self.ser.tx(data)
+			self.tx_x.append(self.tx_count)
+			self.tx_y.append(data)
+			if(self.tx_count > 1000):
+				self.tx_x =  self.tx_x[1:]
+				self.tx_y =  self.tx_y[1:]
+			self.tx_count = self.tx_count + 1
+
+
+	def rx(self):
+		limit = self.ser.wait()
+		for i in range(0,limit):
+			data = self.ser.rx()
+			self.textEditRx.insertPlainText(humanRead(data,text=True))
+			self.textEditRx.ensureCursorVisible()
+			self.rx_x.append(self.rx_count)
+			self.rx_y.append(ord(data))
+			if(self.rx_count > 1000):
+				self.rx_x =  self.rx_x[1:]
+				self.rx_y =  self.rx_y[1:]
+			self.rx_count = self.rx_count + 1
+
+
+	def closeEvent(self, event):
+		self.timer.stop()
+		self.caller.EndGrap()
+
+
+
+
