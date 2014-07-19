4e115f8f0d7204ea2fe499b9a78b4ddfc7af04a7
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-07-19 18:48:14 +0100

    2 train working, needs more testing

--- DebugSerial/dsPy/Plot.py
+++ DebugSerial/dsPy/Plot.py
@@ -25,6 +25,8 @@ class Plot(QtGui.QMainWindow):
 		self.x = []
 		self.y = []
 
+		self.pre = 0x03
+		self.buff = 0x00
 		self.state = "pre"
 		self.show()
 
@@ -39,13 +41,12 @@ class Plot(QtGui.QMainWindow):
 
 	def rx(self):
 		limit = self.ser.wait()
-		pre = 0x03
 		if(limit):
 			for i in range(0,limit):
 				data = ord(self.ser.rx())
 				print self.state
 				if(self.state == "pre"):
-					pre = data
+					self.pre = data
 					if(data & 0x03):
 						if(data & 0x0C):
 							self.x = []			# Clear the graph
@@ -54,12 +55,57 @@ class Plot(QtGui.QMainWindow):
 							self.state = "x1"
 					else:
 						self.state == "pre"		# Failed pre fixed bits
+
+
+
+				# X Axis data
 				elif(self.state == "x1"):
-					self.x.append(data)
+					if(self.pre & 0xC0):
+						self.buff = data
+						self.state = "x2"
+					else:
+						self.x.append(data)
+						self.state = "y1"
+
+				elif(self.state == "x2"):
+					shifted = (self.buff << 8)|data
+					if(self.pre & 0x80):
+						self.buff = shifted
+						self.state = "x3"
+					else:
+						self.x.append(shifted)
+						self.state = "y1"
+
+
+				elif(self.state == "x3"):
+					shifted = (self.buff << 8)|data
+					if(self.pre & 0x40):
+						self.buff = shifted
+						self.state = "x4"
+					else:
+						self.x.append(shifted)
+						self.state = "y1"
+
+				elif(self.state == "x4"):
+					shifted = (self.buff << 8)|data
+					self.append(shifted)
 					self.state = "y1"
+
+
+
+				# Y axis data
 				elif(self.state == "y1"):
-					self.y.append(data)
+					if(self.pre & 0x30):
+						self.buff = data
+						self.state = "y2"
+					else:
+						self.y.append(data)
+						self.state = "pre"
+
+				elif(self.state == "y2"):
+					self.y.append(data << 8)
 					self.state = "pre"
+
 			self.plot.setData(self.x, self.y)
 			self.qwtPlot.replot()
 
